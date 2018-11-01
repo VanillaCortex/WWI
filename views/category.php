66e4a5e617de0242e660627e2b040a840a211258
Thymen Heersmink <thymenheersmink@gmail.com>
2018-11-01 20:35:18 +0100

    paginatie toegevoegd voor producten

--- views/category.php
+++ views/category.php
@@ -7,6 +7,13 @@ if(!isset($_GET) && !isset($_GET['category'])) {
 // Get the category from the GET
 $category = $_GET['category'];
 
+if(isset($_GET['pagination'])) {
+    // Get pagination amount
+    $pagination = $_GET['pagination'];
+} else {
+    $pagination = 25;
+}
+
 // Get the url to do stuff with
 $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 
@@ -22,8 +29,14 @@ WHERE b.StockGroupID = ?) ";
 $products = $pdo->prepare($query);
 $products->execute(array($category));
 
+// Count the amount of products
+$count = $products->rowCount();
+
+// Calculate how many pages we have to be able to scroll through
+$pages = ceil($count / $pagination);
+
 $query = "
-SELECT StockGroupName
+SELECT *
 FROM stockgroups
 WHERE StockGroupID = ?";
 $this_category = $pdo->prepare($query);
@@ -36,7 +49,62 @@ $this_category = $this_category->fetch();
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="../media/css/style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="../media/javascript/jquery.js" type="text/javascript"></script>
     </head>
+    <script>
+
+        // Zodra de browser geladen is, laadt dit in
+        window.onload = function() {
+
+            // Zodra er binnen de body wordt geklikt op een element met een data target genaamd 'change_page'
+            $('body').on('click', '[data-target="change_page"]', function (e) {
+                // Zodra dit gebeurd is dat vuurt de functie 'change_page'
+                change_page(e);
+            });
+
+            function change_page(e) {
+                // Haalt het element op waar op geklikt is
+                var target = $(e.target);
+
+                // Haalt de class active weg bij alle elementen die deze class hebben
+                $('.active').removeClass('active');
+
+                // Haal de id op van het geklikte element
+                var id = target.attr('id');
+
+                // Geeft het element wat om ons geklikt element heen zit de class active
+                $('#' + id).parent().addClass('active');
+
+                // Haalt het tekst gedeelte van de id, omdat deze niet meer nodig is
+                id = id.replace('page_', '');
+
+                // Bereken een aantal variabelen om mee te rekenen
+                var start = (parseInt(id) - 1) * <?=$pagination?>;
+                var end = parseInt(id) * <?=$pagination?>;
+                counter = start;
+
+                // Laat de nieuwe producten van de nieuwe pagina zien
+                while(counter <= end) {
+                    $('#' + counter).removeClass('hidden');
+                    counter++;
+                }
+
+                // Verstop de producten die voor onze selectie zijn
+                counter = start;
+                while(counter > 0) {
+                    counter--;
+                    $('#' + counter).addClass('hidden');
+                }
+
+                // Verstop de producten die na onze selectie zijn
+                counter = end;
+                while(counter < <?=$count?>) {
+                    counter++;
+                    $('#' + counter).addClass('hidden');
+                }
+            }
+        };
+    </script>
     <?php
     // Header
     require_once 'header.php';
@@ -47,14 +115,23 @@ $this_category = $this_category->fetch();
             <div class="col-sm-12">
                 <div class="nice-box">
                     <h3><?= $this_category['StockGroupName'] ?></h3>
+                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                        <div class="btn-group mr-2" role="group" aria-label="First group">
+                            <a href="category.php?category=<?=$this_category['StockGroupID']?>&pagination=25"><button type="button" class="btn btn-secondary">25</button></a>
+                            <a href="category.php?category=<?=$this_category['StockGroupID']?>&pagination=50"><button type="button" class="btn btn-secondary">50</button></a>
+                            <a href="category.php?category=<?=$this_category['StockGroupID']?>&pagination=100"><button type="button" class="btn btn-secondary">100</button></a>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
             <?php
+            $x = 0;
             while($product = $products->fetch()) {
-
+                $x++;
                 ?>
-                <div class="col-sm-3">
+                <div class="col-sm-3 <?php if($x > $pagination) { echo 'hidden';   } ?>" id="<?=$x?>">
                     <div class="nice-box clickable">
                         <p><b><?= $product['StockItemName'] ?></b></p>
                         <?=$product['Photo']?>
@@ -65,5 +142,19 @@ $this_category = $this_category->fetch();
             }
             ?>
         </div>
+
+        <nav aria-label="...">
+            <ul class="pagination pagination-lg">
+                <?php
+                    $page_array = range(1, $pages);
+                    foreach($page_array as $page) {
+                        ?>
+                        <li class="page-item  <?php if($page == 1) { echo 'active'; }?>" data-target="change_page"><a class="page-link" id="page_<?=$page?>"><?=$page?></a></li>
+                        <?php
+                    }
+                ?>
+            </ul>
+        </nav>
+
     </div>
 </html>
