decdc89296734b58fbb8c847c51aeeb87be091c0
frankhulshof <frank.hulshof1@gmail.com>
2018-12-04 14:24:04 +0100

    de pagina voor het bevestigen van de betaling nu is klaar.

--- views/pay.php
+++ views/pay.php
@@ -1,3 +1,64 @@
-<?php
-print("hier moet er betaald worden");
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Betaling succesvol</title>
+    </head>
+    <body>
+        <div class="divb">
+            <div class="container">
+                <h1 class="display-4 head">U heeft succesvol betaald</h1>
+                <hr class="my-4">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur cumque doloremque eius eos fuga harum impedit ipsa mollitia nam natus necessitatibus nemo numquam perferendis quam, reiciendis, repellendus sed tempore?</p>
+        </div><br><br>
+        </div>
+        <div class="diva">
+            <div class="container">
+                <H2 class="head" align="center">Wij zijn:</H2>
+                <hr>
+                <br>
+                <div class="row">
+                    <div class="col-md-4 gridItem">
+                        <i class="fas fa-shipping-fast fa-7x"></i>
+                        <h5>Snel</h5>
+                    </div>
+                    <div class="col-md-4 gridItem">
+                        <i class="far fa-money-bill-alt fa-7x"></i>
+                        <h5>Goedkoop</h5>
+                    </div>
+                    <div class="col-md-4 gridItem">
+                        <i class="far fa-handshake fa-7x"></i>
+                        <h5>Betrouwbaar</h5>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <br>
+            </div>
+        </div>
+
+        <br><br>
+        <div class="container">
+            <h3>Why are we fast?</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut tellus est. Praesent lacinia commodo iaculis. Vestibulum quis ullamcorper ipsum, non eleifend orci. Mauris tincidunt facilisis massa, eu consequat nunc auctor ut. Integer at convallis tellus. Vivamus diam mauris, commodo sit amet molestie eget, efficitur eget felis. Sed a libero sit amet nisi egestas auctor vitae facilisis magna. Duis id volutpat est.
+            </p>
+            <p>
+                Nullam in ullamcorper nisi. Fusce sagittis, lectus eu gravida pellentesque, tellus erat varius justo, nec accumsan lacus eros et justo. Integer aliquet lectus arcu, at ullamcorper mi placerat vel. Vestibulum id euismod sem, vel mattis magna. Sed vitae diam sollicitudin, interdum tellus non, sodales urna. Curabitur rhoncus nisi eget eros malesuada accumsan. Curabitur mollis, ante et vestibulum cursus, sem nulla convallis felis, sed lacinia lectus tortor ut sem. Donec pulvinar purus ut ligula volutpat, eget finibus est fermentum. Nulla sit amet enim iaculis, imperdiet nisi quis, ultrices metus. Maecenas mattis mi eget felis ultrices, sed auctor erat rhoncus. Cras nisi urna, consequat eget auctor nec, viverra vehicula tellus.
+            </p>
+            <h3>Why are we cheap?</h3>
+            <p>
+                Aenean nec laoreet lectus, eget venenatis nunc. Vestibulum interdum neque eget nisi luctus, vel pretium nibh dapibus. Curabitur sodales tempus velit, a mollis tellus fermentum at. Sed pretium tellus ut ipsum tincidunt tincidunt. Donec auctor ipsum risus, vitae faucibus nisl mollis vitae. Suspendisse convallis tortor ac risus iaculis, sit amet bibendum sapien tincidunt. In ac mi sit amet dui semper auctor. Integer posuere fermentum tempor. Phasellus gravida elit sed efficitur congue. Fusce consequat, est sed vestibulum feugiat, erat augue pulvinar orci, vitae tempor lectus dui sit amet leo. Cras eget sapien ligula. Maecenas placerat pretium dui, non semper ante dictum et. Cras ultricies, mi at iaculis rhoncus, lacus lectus hendrerit mauris, suscipit sodales nunc turpis id sem. Nulla facilisi. Aenean bibendum nisl magna, in porttitor libero tristique ac.
+            </p>
+            <p>
+                Etiam quis erat augue. In pharetra sapien a nulla fermentum ullamcorper. Curabitur iaculis sit amet neque ac maximus. Nunc aliquet feugiat nulla, vel fringilla eros sollicitudin et. Aenean nisl mauris, gravida non pharetra eu, bibendum vitae elit. Integer eget convallis nisl. Nullam quis est eu nibh sagittis egestas vel finibus est. Nunc eros dui, sagittis vel lacus sed, vulputate fringilla lacus. Vestibulum porta magna ligula, a porta leo efficitur sit amet. Phasellus id sem ultricies, condimentum sapien quis, viverra augue. Cras nec auctor ante. Vestibulum neque risus, sollicitudin eu accumsan sit amet, lobortis ac tortor. Suspendisse volutpat metus eget odio aliquet mattis. Proin in augue porta, rhoncus lorem a, ultrices ante. Nam eget tellus sit amet tellus tincidunt cursus.
+            </p>
+            <h3>Why trust us?</h3>
+            <p>
+                Proin vel mauris ac velit sagittis placerat. Duis gravida mauris tellus. Integer at augue nec velit viverra consequat vitae a sapien. Vivamus volutpat interdum libero, sed semper quam suscipit eget. Pellentesque pharetra scelerisque arcu, vel fringilla sem. Suspendisse at dui eu turpis dignissim luctus. Fusce eros tortor, convallis id orci quis, ornare semper eros. Donec pharetra leo massa, et lobortis elit scelerisque sit amet. Curabitur cursus vel libero at eleifend.
+            </p>
+            <a class="btn btn-primary btn-lg" href="home" role="button">Ga Terug</a>
+            <br><br><br><br><br>
+        </div>
+    </body>
+
+</html> 
